31462ba8e914cf206e1e3fbd69937a94cc0ba4f5
exigibil <adrian.topa04@gmail.com>
2024-07-31 22:04:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,10 +33,10 @@ export const App = () => {
         <main className={styles.compoenentContainer}>
               <h1>AEM Contact Agenda</h1>
           <Routes>
-            <Route path="/" element={<Navigate to={isLoggedIn ? "/goit-react-hw-07-phonebook" : "/login"} />} />
+            <Route path="/" element={<Navigate to={isLoggedIn ? "/goit-react-hw-08-phonebook" : "/login"} />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/goit-react-hw-07-phonebook" element={isLoggedIn ? <Phonebook /> : <Navigate to="/login" />} />
+            <Route path="/goit-react-hw-08-phonebook" element={isLoggedIn ? <Phonebook /> : <Navigate to="/login" />} />
             <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
         </main>
